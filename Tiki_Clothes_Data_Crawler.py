8277362dd6d026375f86fe432ae73452f2415b49
longITBK <hoangnhatbaolong0215971@gmail.com>
2023-05-20 23:04:26 +0700

    recrawl BigDS with multiples of category and up to 15k samples

--- Tiki_Clothes_Data_Crawler.py
+++ Tiki_Clothes_Data_Crawler.py
@@ -2,6 +2,8 @@ import requests
 import time
 import random
 import pandas as pd
+import os
+import csv
 from retrying import retry
 
 list_products_headers = {
@@ -22,7 +24,7 @@ list_products_params = {
     'category': '915',
     'page': '1',
     'urlKey': 'thoi-trang-nam',
-    'sort' : 'default',
+    'sort' : 'top_seller'
 }
 
 seller_headers = {
@@ -45,11 +47,13 @@ seller_params = {
 
 urlKey_category = {
     'thoi-trang-nam' : '915',
-    'tui-thoi-trang-nam' : '27616',
     # 'giay-dep-nam' : '1686'
     'ao-thun-nam' : '917',
     'ao-so-mi-nam' : '918',
-    'quan-nam' : '27562'
+    'ao-vest-ao-khoac-nam' : '925',
+    'quan-nam' : '27562',
+    'tui-thoi-trang-nam' : '27616',
+    'do-ngu-do-mac-nha-nam' : '27570'
 
 }
 
@@ -80,8 +84,12 @@ def get_products_details(json1, json2):
         print(e)
     return value
 
+csv_file = "HugeDS.csv"
+header = ['id', 'name', 'brand', 'original_price', 'discount', 'current_price', 'discount_rate', 
+          'quantity_sold', 'rating_average', 'product_review_count', 'seller_id', 'seller_name', 
+          'days_since_joined', 'seller_rating_average', 'is_official', 'seller_review_count', 'total_follower']
 products = []
-sample_size = 1000000
+sample_size = 5000
 count = 0
 flag = False
 
@@ -100,43 +108,52 @@ def get_response(url, headers, params):
         # Nếu response lỗi, raise một exception để retry lại
         raise ValueError(f"Request failed with error {response.status_code}. Retrying...")
 
-for key, category in urlKey_category.items():
-    list_products_params['urlKey'] = key
-    list_products_params['category'] = category
-    for s in sort:
-        list_products_params['sort'] = s
-        for i in range(1, 51):
-            list_products_params['page'] = i
-            # response = requests.get('https://tiki.vn/api/personalish/v1/blocks/listings', headers=list_products_headers, params=list_products_params)
-            response = get_response('https://tiki.vn/api/personalish/v1/blocks/listings', headers=list_products_headers, params=list_products_params)
-            requests.adapters.DEFAULT_RETRIES = 10
-            if response.status_code == 200:
-                print('request success!!!')
-                for product in response.json()['data']:
-                    # Call API of Seller
-                    seller_params['seller_id'] = product.get('seller_id')
-                    # response_seller = requests.get('https://tiki.vn/api/shopping/v2/widgets/seller', headers=seller_headers,
-                    #                         params=seller_params)
-                    response_seller = get_response('https://tiki.vn/api/shopping/v2/widgets/seller', headers=seller_headers,
-                                            params=seller_params)
-                    if response_seller.status_code == 200:
-                        value = get_products_details(product, response_seller.json())
-                        if value != None:
-                            products.append(value)
-                            count += 1
-                        if count == sample_size:
-                            flag = True
-                            break
-                print('     ' + str(count) + ' samples collected.')
-                if flag:
-                    break            
-            time.sleep(random.randrange(7, 10))
+
+    # for s in sort:
+    #     list_products_params['sort'] = s
+for i in range(1, 51):
+    list_products_params['page'] = i
+    for key, category in urlKey_category.items():
+        list_products_params['urlKey'] = key
+        list_products_params['category'] = category
+        print('Page: ', i, ' - ', 'urlKey: ', key, ' - ', 'Category: ', category)
+        response = get_response('https://tiki.vn/api/personalish/v1/blocks/listings', headers=list_products_headers, params=list_products_params)        
+        if response.status_code == 200:
+            print('request success!!!')
+            for product in response.json()['data']:
+                # Call API of Seller
+                seller_params['seller_id'] = product.get('seller_id')
+                response_seller = get_response('https://tiki.vn/api/shopping/v2/widgets/seller', headers=seller_headers,
+                                        params=seller_params)
+                if response_seller.status_code == 200:
+                    # Lấy các dữ liệu từ file json khi respond trả về
+                    value = get_products_details(product, response_seller.json())
+                    if value != None:
+                        products.append(value)
+                        count += 1
+                        if not os.path.isfile(csv_file):
+                            # Nếu không tồn tại, tạo tệp mới và ghi tiêu đề vào tệp
+                            with open(csv_file, mode="w", newline="") as csvfile:
+                                writer = csv.writer(csvfile)
+                                writer.writerow(header)
+                        else:
+                            # Nếu đã tồn tại, mở tệp và ghi tiếp dữ liệu vào tệp
+                            with open(csv_file, mode="a", newline="", encoding="utf-8") as csvfile:
+                                writer = csv.writer(csvfile)
+                                writer.writerow(list(value.values()))
+                    if count == sample_size:
+                        flag = True
+                        break
+            print('     ' + str(count) + ' samples collected.')
+            if flag:
+                break            
+        time.sleep(random.randrange(7, 10))
         if flag:
             break
     if flag:
         break
 
-
-df_products = pd.DataFrame(products)
-print(df_products)
-df_products.to_csv('HugeDS.csv', index=False)
+print("Done crawling !!!")
+# df_products = pd.DataFrame(products)
+# print(df_products)
+# df_products.to_csv('HugeDS.csv', index=False)
